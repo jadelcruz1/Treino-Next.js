ba90e6fde71df60673786f22250132de8f541ead
jadelcruz1 <jardelcruz6@gmail.com>
2021-12-13 20:20:20 -0300

    Criando roteamento e paginas no Next.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function About(){
+    return <h1> Página de About! </h1>
+
+}
+
+// OS ARQUIVOS NEXT VÃO GERAR O ROTEAMENTO;
+// Com base no nome do arquivo, há a geração de URL's do projeto.
+// O arquivo index.js sinaliza a criação da rota base.
+//por exemplo : https://www.localhost:3000/about = index.js
+// este arquivo  deve está fora da pasta ./pages/api.
+//deve pertencer a pasta pages.
---
+++ pages/products.js
@@ -0,0 +1,14 @@
+export default function Products(){
+    return (
+        <>
+            <h1> Teste de renderização</h1>
+
+            <p> teste de fragment em Next.js</p>
+
+        </>
+    )
+}
+
+// mesma regra do React.js quando tiver mais de uma linha no render, criar uma div ou fragment.
+//nome da função sempre a primeira letra maíuscula, porém o arquivo em minusculo.
+//para acessar a pagina na web usar o nome do arquivo(letra minuscula), não da function (letra maíuscula)
